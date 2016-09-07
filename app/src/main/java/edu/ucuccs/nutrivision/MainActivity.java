b0e0feafd25c025aa4beded9c2767f75b51b7b48
Byron Mairina <Byron Mairina>
2016-09-07 17:06:21 +0800

    Placed image on Second Activity

--- app/src/main/java/edu/ucuccs/nutrivision/MainActivity.java
+++ app/src/main/java/edu/ucuccs/nutrivision/MainActivity.java
@@ -1,12 +1,22 @@
 package edu.ucuccs.nutrivision;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.afollestad.materialcamera.MaterialCamera;
 import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private final static int RESULT_LOAD_IMAGE = 1;
 
     FloatingActionButton mFabCam, mFabBrowse;
+    FloatingActionMenu fabMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +33,14 @@ public class MainActivity extends AppCompatActivity {
 
         mFabCam = (FloatingActionButton) findViewById(R.id.menu_camera);
         mFabBrowse = (FloatingActionButton) findViewById(R.id.menu_browse);
+        fabMenu = (FloatingActionMenu)findViewById(R.id.fab_menu);
 
 
         mFabCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cameraShot();
+                fabMenu.close(true);
             }
         });
 
@@ -35,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 browseGal();
+                fabMenu.close(true);
             }
         });
     }
@@ -59,4 +73,45 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(i, RESULT_LOAD_IMAGE);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_RQ) {
+            if (resultCode == RESULT_OK) {
+                File file = new File(data.getData().getPath());
+                Intent i = new Intent(this, SecondActivity.class);
+                i.putExtra("image", file);
+                startActivity(i);
+
+            } else if (data != null) {
+                Exception e = (Exception) data.getSerializableExtra(MaterialCamera.ERROR_EXTRA);
+                if (e != null) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            File file = new File(picturePath);
+
+            Intent i = new Intent(this, SecondActivity.class);
+            i.putExtra("image", file);
+            startActivity(i);
+
+        }
+
+    }
 }
--- app/src/main/java/edu/ucuccs/nutrivision/SecondActivity.java
+++ app/src/main/java/edu/ucuccs/nutrivision/SecondActivity.java
@@ -5,68 +5,27 @@
 
 package edu.ucuccs.nutrivision;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
+
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.afollestad.materialcamera.MaterialCamera;
 
 import java.io.File;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private final static int CAMERA_RQ = 6969;
-    private final static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        File pictureFile = (File)getIntent().getExtras().get("image");
+        Uri imageUri = Uri.fromFile(pictureFile);
+        ImageView imageView = (ImageView)findViewById(R.id.scanImage);
+        imageView.setImageURI(imageUri);
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == CAMERA_RQ) {
-            if (resultCode == RESULT_OK) {
-                final File file     = new File(data.getData().getPath());
-                Uri        imageUri = Uri.fromFile(file);
-                ImageView  image    = (ImageView) findViewById(R.id.scanImage);
-                image.setImageURI(imageUri);
-            } else if (data != null) {
-                Exception e = (Exception) data.getSerializableExtra(MaterialCamera.ERROR_EXTRA);
-                if (e != null) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-
-        }
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            ImageView image = (ImageView) findViewById(R.id.scanImage);
-            image.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-
-        }
-
-    }
 }
