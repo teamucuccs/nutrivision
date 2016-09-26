789fa3a9ac0dd6f561618bf90c27e34e938896a2
ajdeguzman <aljohndeguzman@gmail.com>
2016-09-26 21:12:31 +0800

    finalized for first release of app

--- app/src/main/java/edu/ucuccs/nutrivision/MainActivity.java
+++ app/src/main/java/edu/ucuccs/nutrivision/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity{
                 fabMenu.close(true);
             }
         });
-        featureDiscovery();
+        //featureDiscovery();
     }
 
     void featureDiscovery(){
